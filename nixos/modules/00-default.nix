69f8824f07b28db664b8340b6b9bf740d132887e
Nate Currit <ncurrit@gmail.com>
2026-05-30 17:35:50 -0500

    Move cache config to nix.settings for system-wide trust

--- nixos/modules/00-default.nix
+++ nixos/modules/00-default.nix
@@ -11,7 +11,17 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    trusted-substituters = [
+      "https://noctalia.cachix.org"
+      "https://niri.cachix.org"
+    ];
+    trusted-public-keys = [
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+      "niri.cachix.org-1:EjH3R0Yj5t5pJbM1rWvQ3L8xK9yC4h7fA6eB2sD5gU0="
+    ];
+  };
 
   time.timeZone = "TIMEZONE";
 

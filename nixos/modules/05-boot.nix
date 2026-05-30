85b718da74e42eb0d156d825091cd2683bc6020c
Nate Currit <ncurrit@gmail.com>
2026-05-30 17:15:53 -0500

    Add systemd-boot bootloader config

--- nixos/modules/05-boot.nix
+++ nixos/modules/05-boot.nix
@@ -1,6 +1,9 @@
 { ... }:
 
 {
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   # Memory optimization: ZRAM and swappiness
   zramSwap.enable = true;
   boot.kernel.sysctl."vm.swappiness" = 10;

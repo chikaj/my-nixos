6b8c0101fc4efbb60702ee044f25ca41d525b6da
Nate Currit <ncurrit@gmail.com>
2026-06-01 16:57:32 -0500

    Enable NetworkManager to prevent DHCP boot delay

--- nixos/modules/03-services.nix
+++ nixos/modules/03-services.nix
@@ -5,6 +5,9 @@
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   xdg.portal.config.common.default = [ "*" ];
 
+  # Network management
+  networking.networkmanager.enable = true;
+
   # Required services for Noctalia
   services.power-profiles-daemon.enable = true;
   services.upower.enable = true;

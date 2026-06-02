9eb2f79d3e8749a9876272fa7f49b2f7f97f1793
Nate Currit <ncurrit@gmail.com>
2026-06-02 11:30:59 -0500

    add fancy-audiovisualizer and port-monitor plugins

--- home/noctalia.nix
+++ home/noctalia.nix
@@ -7,5 +7,26 @@
 
   programs.noctalia-shell = {
     enable = true;
+
+    plugins = {
+      sources = [
+        {
+          enabled = true;
+          name = "Noctalia Plugins";
+          url = "https://github.com/noctalia-dev/noctalia-plugins";
+        }
+      ];
+      states = {
+        fancy-audiovisualizer = {
+          enabled = true;
+          sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+        };
+        port-monitor = {
+          enabled = true;
+          sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+        };
+      };
+      version = 2;
+    };
   };
 }

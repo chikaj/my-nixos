a84ece34643b26139421724de575cba23b5ac83f
Nate Currit <ncurrit@gmail.com>
2026-06-04 11:25:46 -0500

    removed erroneous blur

--- home/niri.nix
+++ home/niri.nix
@@ -17,9 +17,6 @@ in
         {
           geometry-corner-radius = { top-left = 20.0; top-right = 20.0; bottom-right = 20.0; bottom-left = 20.0; };
           clip-to-geometry = true;
-          background-effect = {
-            blur = true;
-          };
         }
       ];
 
